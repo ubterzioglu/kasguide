dd9b9ddae442d7c0391e05dc283050e5bceb3b42
ubterzioglu <ubterzioglu@gmail.com>
2025-12-26 15:52:33 +0100

    BIG FOLDER CHANGE!

--- categories.js
+++ categories.js
@@ -33,7 +33,7 @@ const categories = [
   },
 {
   id: 'food',
-  name: 'Yeme-İçme',
+  name: 'Restoran',
   icon: `<svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
     <!-- Çatal -->
     <path d="M4 3v7"/>
